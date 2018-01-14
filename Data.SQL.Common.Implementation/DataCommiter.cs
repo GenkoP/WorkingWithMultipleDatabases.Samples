cae18b320e46e7fa092e115691a45d325b9c9c20
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 16:51:00 +0200

    Add DataCommiter object which will build sql command and execute it

---
+++ Data.SQL.Common.Implementation/DataCommiter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Data.Common.Contracts;
+using Data.SQL.Common.Contracts;
+
+namespace Data.SQL.Common.Implementation
+{
+    public sealed class DataCommiter : IDataCommiter
+    {
+        private readonly ICommandBuilder commandBuilder;
+        private readonly IDataCommandExecuter dataCommandExecuter;
+
+        public DataCommiter(ICommandBuilder commandBuilder, IDataCommandExecuter dataCommandExecuter)
+        {
+            this.commandBuilder = commandBuilder ?? throw new ArgumentNullException($"{nameof(commandBuilder)} should not be null");
+            this.dataCommandExecuter = dataCommandExecuter ?? throw new ArgumentNullException($"{nameof(dataCommandExecuter)} should not be null");
+        }
+
+        public void Commit()
+        {
+            string sqlCommand = this.commandBuilder.Build();
+            if (!string.IsNullOrWhiteSpace(sqlCommand))
+            {
+                this.dataCommandExecuter.Execute(sqlCommand);
+            }
+        }
+    }
+}
