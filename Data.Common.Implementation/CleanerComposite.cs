1011eba696353db89c4d7a623f9def0c2c78ac8d
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 14:03:56 +0200

    Add Data.Common.Implementation project which implement common contracts for all databases

---
+++ Data.Common.Implementation/CleanerComposite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Data.Common.Contracts;
+
+namespace Data.Common.Implementation
+{
+    public sealed class CleanerComposite : ICleanable
+    {
+        private readonly IEnumerable<ICleanable> cleaners;
+
+        public CleanerComposite(IEnumerable<ICleanable> cleaners)
+        {
+            this.cleaners = cleaners ?? throw new NullReferenceException("Cleaners collection should not be null");
+        }
+
+        public void Clean()
+        {
+            foreach (ICleanable cleaner in cleaners)
+            {
+                cleaner.Clean();
+            }
+        }
+    }
+}
---
+++ Data.Common.Implementation/TemporaryInMemoryDataStore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Data.Common.Contracts;
+
+namespace Data.Common.Implementation
+{
+    public sealed class TemporaryInMemoryDataStore<TKey, TEntity>
+        : ITemporaryInMemoryDataStore<TKey, TEntity>, ICleanable
+        , IEntitiesForInsertingProvider<TEntity>, IEntitiesForRemovingProvider<TKey,TEntity>
+        where TEntity : class
+    {
+        private readonly ICollection<TKey> keysForRemove;
+        private readonly ICollection<TEntity> entitiesForInsertion;
+
+        public TemporaryInMemoryDataStore()
+        {
+            keysForRemove = new HashSet<TKey>();
+            entitiesForInsertion = new List<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetAllEntitiesForInserting()
+        {
+            return this.entitiesForInsertion;
+        }
+
+        public IEnumerable<TKey> GetAllEntitiesForRemoving()
+        {
+            return this.keysForRemove;
+        }
+
+        public void AppendForInserting(TEntity entity)
+        {
+            this.entitiesForInsertion.Add(entity);
+        }
+
+        public void AppendForRemoving(TKey key)
+        {
+            this.keysForRemove.Add(key);
+        }
+
+        public void Clean()
+        {
+            this.keysForRemove.Clear();
+            this.entitiesForInsertion.Clear();
+        }
+    }
+}
