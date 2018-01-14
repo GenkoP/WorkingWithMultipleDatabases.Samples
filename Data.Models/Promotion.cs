9a993ce8e3fa83fc99c99c7cbfd29ac8fdae0488
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 11:17:05 +0200

    Add Data.Models project with 2 simple models User and Promotion without relation

---
+++ Data.Models/Promotion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data.Models
+{
+    public class Promotion
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Data.Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
