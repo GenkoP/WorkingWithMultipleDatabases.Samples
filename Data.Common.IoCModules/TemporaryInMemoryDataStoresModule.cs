c1add4854d3cbe8a9dcb7ed5d2e17d2958796a9c
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 14:29:09 +0200

    Add Data.Common.IoCModule and register temporary in memory data store objects for User and Promotion data models

---
+++ Data.Common.IoCModules/TemporaryInMemoryDataStoresModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Data.Common.Contracts;
+using Data.Common.Implementation;
+using Data.Models;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace Data.Common.IoCModules
+{
+    public sealed class TemporaryInMemoryDataStoresModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            Type[] temporaryInMemorydataStoreTypes = new Type[]
+            {
+                typeof(TemporaryInMemoryDataStore<Guid, User>),
+                typeof(TemporaryInMemoryDataStore<Guid, Promotion>),
+            };
+
+            container.RegisterCollection<ICleanable>(temporaryInMemorydataStoreTypes);
+            container.Register<ICleanable, CleanerComposite>();
+
+            container.Register<TemporaryInMemoryDataStore<Guid, User>, TemporaryInMemoryDataStore<Guid, User>>();
+            container.Register<TemporaryInMemoryDataStore<Guid, Promotion>, TemporaryInMemoryDataStore<Guid, Promotion>>();
+
+            container.Register<IEntitiesForInsertingProvider<User>, TemporaryInMemoryDataStore<Guid, User>>();
+            container.Register<IEntitiesForInsertingProvider<Promotion>, TemporaryInMemoryDataStore<Guid, Promotion>>();
+
+            container.Register<IEntitiesForRemovingProvider<Guid, User>, TemporaryInMemoryDataStore<Guid, User>>();
+            container.Register<IEntitiesForRemovingProvider<Guid, Promotion>, TemporaryInMemoryDataStore<Guid, Promotion>>();
+        }
+    }
+}
