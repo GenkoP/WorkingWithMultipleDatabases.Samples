56067a7dba7e82b711729b1d6a1a69cf88b5649c
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 16:31:39 +0200

    Add Data.SQL.Common.IoCModule project and registrations for all command builders.

---
+++ Data.SQL.Common.IoCModule/CommanBuildersModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Data.SQL.Common.Contracts;
+using Data.SQL.Common.Implementation.CommandBuilders;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace Data.SQL.Common.IoCModule
+{
+    public sealed class CommanBuildersModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            Assembly assembly = typeof(CommandBuilderComposite).Assembly;
+            container.RegisterCollection<ICommandBuilder>(new[] { assembly });
+            container.Register<ICommandBuilder, CommandBuilderComposite>();
+        }
+    }
+}
