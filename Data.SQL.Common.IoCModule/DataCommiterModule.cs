d79ba85777113dc026a02c5e90d0a404449b1f33
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 16:51:31 +0200

    Add data commiter module which register DataCommiter object

---
+++ Data.SQL.Common.IoCModule/DataCommiterModule.cs
@@ -0,0 +1,15 @@
+﻿using Data.Common.Contracts;
+using Data.SQL.Common.Implementation;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace Data.SQL.Common.IoCModule
+{
+    public sealed class DataCommiterModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IDataCommiter, DataCommiter>();
+        }
+    }
+}
