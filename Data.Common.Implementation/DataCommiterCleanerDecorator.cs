203d3c9a836a101ae2fbdbe447649d14c9eb30cd
Genko Petrov <genko_petrov@hotmail.com>
2018-01-15 00:08:06 +0200

    Add data commiter decorator which clean all temporaty data stores after commit

---
+++ Data.Common.Implementation/DataCommiterCleanerDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Data.Common.Contracts;
+
+namespace Data.Common.Implementation
+{
+    public sealed class DataCommiterCleanerDecorator : IDataCommiter
+    {
+        private readonly ICleanable cleaner;
+        private readonly IDataCommiter dataCommiter;
+
+        public DataCommiterCleanerDecorator(ICleanable cleaner, IDataCommiter dataCommiter)
+        {
+            this.cleaner = cleaner ?? throw new ArgumentNullException($"{nameof(cleaner)} should not be null");
+            this.dataCommiter = dataCommiter ?? throw new ArgumentNullException($"{nameof(dataCommiter)} should not be null");
+        }
+
+        public void Commit()
+        {
+            this.dataCommiter.Commit();
+            this.cleaner.Clean();
+        }
+    }
+}
---
+++ Data.Common.IoCModules/DataCommiterCleanerDecoratorModule.cs
@@ -0,0 +1,15 @@
+﻿using Data.Common.Contracts;
+using Data.Common.Implementation;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace Data.Common.IoCModules
+{
+    public sealed class DataCommiterCleanerDecoratorModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.RegisterDecorator<IDataCommiter, DataCommiterCleanerDecorator>();
+        }
+    }
+}
