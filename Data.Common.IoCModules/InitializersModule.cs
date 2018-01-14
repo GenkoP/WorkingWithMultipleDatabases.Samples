104072ffb7d110d101b9ba268a18dd57ede03993
Genko Petrov <genko_petrov@hotmail.com>
2018-01-14 23:30:31 +0200

    Add initialiazers module.

---
+++ Data.Common.IoCModules/InitializersModule.cs
@@ -0,0 +1,14 @@
+﻿using SimpleInjector;
+using SimpleInjector.Packaging;
+using Utility.Contracts;
+
+namespace Data.Common.IoCModules
+{
+    public sealed class InitializersModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.RegisterCollection<IInitializer>();
+        }
+    }
+}
