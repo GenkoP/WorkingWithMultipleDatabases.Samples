5b97cd6eb872693ce93da481cba0d1d1d05a232f
Genko Petrov <genko_petrov@hotmail.com>
2018-01-15 21:22:36 +0200

    Add Data.SQL.Dapper.Implementation project and implement dapper IDataCommandExecuter

---
+++ Data.SQL.Dapper.Implementation/DataCommandExecuter.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Data.SQL.Common.Contracts;
+
+namespace Data.SQL.Dapper.Implementation
+{
+    public sealed class DataCommandExecuter : IDataCommandExecuter
+    {
+        private readonly IDbConnection dbConnection;
+
+        public DataCommandExecuter(string connectionStringName)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+            dbConnection = new SqlConnection(connectionString);
+        }
+
+        public void Execute(string dataCommand)
+        {
+            dbConnection.Execute(dataCommand);
+        }
+    }
+}
