18c6298a67cdcaabf13e8736f0f50b3e0019b71a
Genko Petrov <genko_petrov@hotmail.com>
2018-01-17 23:33:01 +0200

    Add chech for constructor arguments

--- Data.NoSQL.MongoDB.Implementation/MongoDbConnection.cs
+++ Data.NoSQL.MongoDB.Implementation/MongoDbConnection.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using System;
+using MongoDB.Driver;
 
 namespace Data.NoSQL.MongoDB.Implementation
 {
@@ -8,6 +9,16 @@ namespace Data.NoSQL.MongoDB.Implementation
 
         public MongoDbConnection(string connectionString, string dataBaseName)
         {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException($"{nameof(connectionString)} should not be null or white-space");
+            }
+
+            if (string.IsNullOrWhiteSpace(dataBaseName))
+            {
+                throw new ArgumentException($"{nameof(dataBaseName)} should not be null or white-space");
+            }
+
             MongoClient client = new MongoClient(connectionString);
             database = client.GetDatabase(dataBaseName);
         }
