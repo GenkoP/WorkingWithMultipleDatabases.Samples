518c4aefa718713d52ec980ad75a80037ac24ab6
Genko Petrov <genko_petrov@hotmail.com>
2018-01-17 23:32:06 +0200

    Add Data.NoSQL.MongoDB.IoCModule project and register dependencies

---
+++ Data.NoSQL.MongoDB.IoCModule/DataMongoDBModule.cs
@@ -0,0 +1,20 @@
+﻿using Data.NoSQL.MongoDB.Implementation;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+using Utility.Contracts;
+
+namespace Data.NoSQL.MongoDB.IoCModule
+{
+    public sealed class DataMongoDBModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<MongoDbConnection>(() =>
+            {
+                IConfigurationProvider configurationProvider = container.GetInstance<IConfigurationProvider>();
+                string connectionString = configurationProvider.GetConnectionString("MongoDbTestMultipleDb");
+                return new MongoDbConnection(connectionString, "testmultipledb");
+            });
+        }
+    }
+}
---
+++ Data.NoSQL.MongoDB.IoCModule/MongoDBDataCommiterModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Data.Common.Contracts;
+using Data.Common.Implementation;
+using Data.Models;
+using Data.NoSQL.MongoDB.Implementation.DataCommiters;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace Data.NoSQL.MongoDB.IoCModule
+{
+    public sealed class MongoDBDataCommiterModule : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.RegisterCollection<IDataCommiter>(new[]
+            {
+                typeof(InsertDataCommiter<User>),
+                typeof(InsertDataCommiter<Promotion>),
+                typeof(RemoveDataCommiter<Guid, User>),
+                typeof(RemoveDataCommiter<Guid, Promotion>)
+            });
+
+            container.Register<IDataCommiter, DataCommiterComposite>();
+        }
+    }
+}
