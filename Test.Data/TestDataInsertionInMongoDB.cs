5fa229ca4be40d6d99b8d72f75d9551bfefc4745
Genko Petrov <genko_petrov@hotmail.com>
2018-01-17 23:54:48 +0200

    Add test for data insertion in MongoDB

---
+++ Test.Data/TestDataInsertionInMongoDB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Data.Common.IoCModules;
+using Data.Models;
+using Data.NoSQL.MongoDB.IoCModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleInjector.Packaging;
+
+namespace Test.Data
+{
+    [TestClass]
+    public class TestDataInsertionInMongoDB : BaseTest
+    {
+        protected override IPackage[] RegisterPackages()
+        {
+            IPackage[] packages = new IPackage[]
+            {
+                new TemporaryInMemoryDataStoresModule(),
+                new DataMongoDBModule(),
+                new MongoDBDataCommiterModule(),
+            };
+
+            return packages;
+        }
+
+        [TestMethod]
+        public void InsertUser()
+        {
+            User user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Username = "Baba Meca",
+                Password = "baba@meca123",
+                Email = "baba@meca.com"
+            };
+
+            base.userDataStore.AppendForInserting(user);
+            base.dataCommiter.Commit();
+        }
+
+        [TestMethod]
+        public void InsertPromotion()
+        {
+            Promotion promotion = new Promotion()
+            {
+                Id = Guid.NewGuid(),
+                Name = "promo baba meca",
+                CategoryName = "TV"
+            };
+
+            base.promotionDataStore.AppendForInserting(promotion);
+            base.dataCommiter.Commit();
+        }
+
+        [TestMethod]
+        public void InsetUserAndPromotion()
+        {
+            User user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Username = "Baba Meca",
+                Password = "baba@meca123",
+                Email = "baba@meca.com"
+            };
+
+            Promotion promotion = new Promotion()
+            {
+                Id = Guid.NewGuid(),
+                Name = "promo baba meca",
+                CategoryName = "TV"
+            };
+
+            base.userDataStore.AppendForInserting(user);
+            base.promotionDataStore.AppendForInserting(promotion);
+
+            base.dataCommiter.Commit();
+        }
+    }
+}
